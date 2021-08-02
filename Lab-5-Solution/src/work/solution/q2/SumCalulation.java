926f87b72705423e8808bc9d0181d828ac708fa3
Himanshu Judge <jhimanshu9922@gmail.com>
2021-08-02 11:34:48 +0530

    Improved solution of Lab-5 Q2

--- Lab-5-Solution/src/work/solution/q2/SumCalulation.java
+++ Lab-5-Solution/src/work/solution/q2/SumCalulation.java
@@ -1,52 +1,48 @@
 package work.solution.q2;
-class Runnable{
-	private int number;
-	
-	public Runnable() {
-		this.number=(int)(Math.random()*10+1);	
-	}
+class Job implements Runnable{
 
-	public int getNumber() {
-		return number;
-	}
-}
-class MyThread extends Thread{
+	private int randNumber;
 	
-	private Runnable randomNumber;
+	public Job() {
+		this.randNumber=(int)(Math.random()*10+1);	;
+	}
 	
 	@Override
 	public void run() {
-		randomNumber=new Runnable();
-		System.out.println("start :==>"+Thread.currentThread().getName());
-		System.out.println("--------randomNumber :"+randomNumber.getNumber()+"--------");
-		System.out.println("end :==>"+Thread.currentThread().getName());
+		System.out.println("start :==> "+Thread.currentThread().getName());
+		System.out.println(Thread.currentThread().getName()+" , randnumber :==>"+this.randNumber);
+		System.out.println("end  :==> "+Thread.currentThread().getName());
 	}
 
 	public int getRandomNumber() {
-		return randomNumber.getNumber();
+		return this.randNumber;
 	}
 	
 }
 public class SumCalulation {
 	public static void main(String[] args) {
 		
-		System.out.println("start :"+Thread.currentThread().getName());
+System.out.println("start :"+Thread.currentThread().getName());
 		
-	
-		MyThread thread1=new MyThread();
-		MyThread thread2=new MyThread();
-		MyThread thread3=new MyThread();
-		MyThread thread4=new MyThread();
-		MyThread thread5=new MyThread();
+		Job job1=new Job();
+		Job job2=new Job();
+		Job job3=new Job();
+		Job job4=new Job();
+		Job job5=new Job();
 		
-		MyThread array[]= {thread1,thread2,thread3,thread4,thread5};
+		Job jobArray[]= {job1,job2,job3,job4,job5};
+		
+		Thread thread1=new Thread(job1,"thread1");
+		Thread thread2=new Thread(job2,"thread2");
+		Thread thread3=new Thread(job3,"thread3");
+		Thread thread4=new Thread(job4,"thread4");
+		Thread thread5=new Thread(job5,"thread5");
 		
 		thread1.start();
 		thread2.start();
 		thread3.start();
 		thread4.start();
 		thread5.start();
-		
 		
 		try {
 			thread1.join();
@@ -58,12 +54,14 @@ public class SumCalulation {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("end: "+Thread.currentThread().getName());
-		int sum=0;
 		
-		for(MyThread thread:array) {
-			sum+=thread.getRandomNumber();
+		
+		System.out.println("end :"+Thread.currentThread().getName());
+		int sum=0;
+		for(Job job:jobArray) {
+			sum+=job.getRandomNumber();
 		}
-		System.out.println("Total sum :"+sum);
+	
+		System.out.println("Random number sum: "+sum);
 	}
 }
